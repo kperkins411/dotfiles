251990eb9a3dd4438f1e0d5126039dd968bf00e5
Keith Perkins <keith.perkins@cnu.edu>
2023-05-01 12:28:57 -0400

    lecture

--- setup.sh
+++ setup.sh
@@ -3,25 +3,31 @@
 #where the configuration files come from
 DOTFILES_URL=https://github.com/kperkins411/dotfiles.git
 LOC=.cfg  #where config files go
+TMP=tmp999 #where to clone setup files
 NAME='Keith Perkins'
 EMAIL='keith.perkins@cnu.edu'
 
 #clone repo into ~/.cfg, --depth=1 gets only last commits (faster)
-git clone --depth=1 $DOTFILES_URL ~/$LOC/
+git clone --depth=1 $DOTFILES_URL ~/$TMP/
 
 #if existing config files exist 
-for p in .vimrc .tmux.conf .ctags .gitconfig
+for p in .vimrc .tmux.conf .ctags .gitconfig .bash_aliases
 do
     #if there remove it
     if test ~/$p; then
         rm ~/$p 
+        echo "Removing /$p" 
     fi
 
+    cp ~/$TMP/$p ~/$LOC/$p
+    echo "Copying $p to $LOC" 
+     
     #symlink new config file
     ln -s ~/$LOC/$p ~/
 done
 
 #add any aliases
+
 chmod 766 ~/$LOC/.bash_aliases 
 source ~/$LOC/.bash_aliases
 
